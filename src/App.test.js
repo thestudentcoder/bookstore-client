c70cbedd57b6114cc097c74ddd64189964a384b2
Wayne Chan <wchan.it@outlook.com>
2021-06-09 04:55:34 -0500

    cicd setup and deploy to heroku

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from "@testing-library/react"
+import App from "./components/App";
+
+describe('App component', () => {
+    it('should render app without error', () => {
+        const { asFragment } = render(<App />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
